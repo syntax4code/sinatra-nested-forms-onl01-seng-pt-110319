c8072192af893896b9dd51b730048be409f2396b
Warren Harper II <warrenharper2@att.net>
2020-02-21 00:49:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,22 +6,21 @@ module FormsLab
     get '/' do
          erb :root
        end
-   
-   
+
+
        get '/new' do
          erb :'pirates/new'
        end
-   
+
        post "/pirates" do
          @pirate = Pirate.new(params[:pirate])
-   
+
          params[:pirate][:ships].each do |details|
            Ship.new(details)
          end
          @ships = Ship.all
-   
+
          erb :'pirates/show'
        end
-     end	 
+     end
    end
-
